791ecdd859591cca9b782e523630de3a1517cac9
siaxvii <lasya.yakkala@gmail.com>
2023-10-21 17:17:15 -0500

    adds navbar and changes background color

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <p>Yippee :D</p>
       <Outlet/>
     </div>
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import "../styles/Navbar.css";
+
+//Renders the top navigation bar of the admin console
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbarWrapper">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
