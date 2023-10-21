6860b11736f3435f4a1a4c809ee58d7ea067cddc
xwilson03 <xanderwilson2014@gmail.com>
2023-10-21 12:26:03 -0500

    fixes express backend after move from AWS to local

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const http    = require('http');
-const https   = require('https');
-const fs      = require('fs');
+// const http    = require('http');
+// const https   = require('https');
+// const fs      = require('fs');
 
 const PORT = 4000;
 
-const certificate = fs.readFileSync('/etc/ssl/certificate.crt', 'utf8');
-const privateKey = fs.readFileSync('/etc/ssl/private.key', 'utf8');
-const credentials = {key: privateKey, cert: certificate};
+// const certificate = fs.readFileSync('/etc/ssl/certificate.crt', 'utf8');
+// const privateKey = fs.readFileSync('/etc/ssl/private.key', 'utf8');
+// const credentials = {key: privateKey, cert: certificate};
 
 const app = express();
 
@@ -15,5 +15,7 @@ app.get("/api/", (req, res) => {
   res.send({"heyo": "skibidi"});
 });
 
-const httpsServer = https.createServer(credentials, app);
-httpsServer.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
+// const httpsServer = https.createServer(credentials, app);
+// httpsServer.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
