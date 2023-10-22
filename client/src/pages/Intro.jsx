7c714e26a132c5bcccd17df026a5eb0b8d49d73e
Christian Walker <christiandwalker2021@gmail.com>
2023-10-21 21:17:27 -0500

    Added intro button

--- client/src/pages/Intro.jsx
+++ client/src/pages/Intro.jsx
@@ -1,11 +1,21 @@
 import "../styles/intro.css";
+import "../styles/App.css";
+import {Link} from 'react-router-dom';
+
 
 function Intro() {
   return (
     <div className="Intro">
-      <div id="optimeal_intro">
-        <h1>OptiMeal</h1>
-      </div>
+        <div className="content_div">
+          <h1 className="optimeal_text">OptiMeal</h1>
+          <div className="planner_div">
+            <h2 className="planner_text">Your personalized meal planner.</h2>
+          </div>
+      {/* <div className="get_started"> */}
+        <Link to="/Planner">
+          <button  className="get_started_button">Get Started</button>
+        </Link>
+        </div>
     </div>
   );
 }
