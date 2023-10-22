535e70773826d7a29face656778aae5da0bb135c
xwilson03 <xanderwilson2014@gmail.com>
2023-10-22 10:54:45 -0500

    adds recipes to browse

--- client/src/pages/Browse.jsx
+++ client/src/pages/Browse.jsx
@@ -1,8 +1,31 @@
 import "../styles/App.css";
 import Sidebar from "../components/Sidebar.jsx";
-import CardGrid from "../components/CardGrid.jsx";
 import Search from "../components/Search.jsx";
-import { Stack } from '@mui/material';
+import { Card, CardActionArea, Stack, Grid } from '@mui/material';
+import axios from 'axios'
+
+const cardData = await axios.get("/api/recipes")
+    .then( (res) => {
+        return res.data;
+    });
+
+const CardGrid = () => {
+    return (
+      <Grid container spacing={3} columns={4}>
+        {cardData.map((card, index) => (
+            <Grid item md={1} key={index}>
+              <Card className="browseCard">
+                <CardActionArea>
+                  <a href={card.sourceUrl}>
+                    {card.title}
+                  </a>
+                </CardActionArea>
+              </Card>
+            </Grid>
+        ))}
+      </Grid>
+    );
+  };
 
 function Browse() {
 
