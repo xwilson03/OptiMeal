dd7fef1d5184805fcde56afa1cfa34ffc70bcb22
Christian Walker <christiandwalker2021@gmail.com>
2023-10-21 21:15:59 -0500

    Added NoPage

--- client/src/pages/NoPage.jsx
+++ client/src/pages/NoPage.jsx
@@ -1,9 +1,11 @@
 import "../styles/App.css";
-
+import error_img from "../assets/hungry_mane.png";
 function NoPage() {
   return (
     <div className="NoPage">
-      <p>NoPage</p>
+      <h1>404</h1>
+      <img id="error_image" src={error_img} alt="error_image" style={{height:'10%',width:'20%'}}/>
+      <h1>Uh Oh! That Page Doesn't Exist!</h1>
     </div>
   );
 }
