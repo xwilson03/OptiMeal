819428949d1351a0fbc806923c9cce90d4889266
xwilson03 <xanderwilson2014@gmail.com>
2023-10-22 10:15:13 -0500

    fixes model and express routing

--- server/routes/Recipe.js
+++ server/routes/Recipe.js
@@ -1,17 +1,16 @@
-const recipe = require("../models/recipe");
+const Recipe = require("../models/Recipe");
 const router = require("express").Router();
-//Change to recipes
+
 router.get("/", async (req, res) => {
   const query = req.query.new;
   try {
     const recipes = query
-      ? await basicModel.find().sort({ _id: -1 }).limit(5)
-      : await basicModel.find();
+      ? await Recipe.find().sort({ _id: -1 }).limit(5)
+      : await Recipe.find();
     res.status(200).json(recipes);
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
 
 module.exports = router;
