cd607d5923a4d723c30497e6e8a17db1166b48c8
xwilson03 <xanderwilson2014@gmail.com>
2023-10-22 00:03:23 -0500

    simplifies intro page

--- client/src/pages/Intro.jsx
+++ client/src/pages/Intro.jsx
@@ -1,22 +1,18 @@
-import "../styles/intro.css";
+import "../styles/Intro.css";
 import "../styles/App.css";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Container } from '@mui/material';
 
 
 function Intro() {
   return (
-    <div className="Intro">
-        <div className="content_div">
-          <h1 className="optimeal_text">OptiMeal</h1>
-          <div className="planner_div">
-            <h2 className="planner_text">Your personalized meal planner.</h2>
-          </div>
-      {/* <div className="get_started"> */}
-        <Link to="/Planner">
-          <button  className="get_started_button">Get Started</button>
-        </Link>
-        </div>
-    </div>
+    <Container className="intro">
+      <h1>OptiMeal</h1>
+      <h2>Your personalized meal planner.</h2>
+      <Link to="/Planner">
+        <button className="get_started_button">Get Started</button>
+      </Link>
+    </Container>
   );
 }
 
