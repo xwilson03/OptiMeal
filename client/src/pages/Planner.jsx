c2cf81c13dc5193bec2ba9e64b3ae906c6ac34af
xwilson03 <xanderwilson2014@gmail.com>
2023-10-22 08:14:20 -0500

    adds meal cards to planner

--- client/src/pages/Planner.jsx
+++ client/src/pages/Planner.jsx
@@ -1,24 +1,49 @@
 import { Card, Stack} from "@mui/material"
+import { State, useState } from "react"
 import "../styles/Planner.css"
 
+const defaultMeals = {"breakfast": "default-breakfast",
+                      "lunch":     "default-lunch",
+                      "dinner":    "default-dinner"};
+
+
 function DayCard(props) {
+
+    const [meals, setMeals] = useState(defaultMeals);
+
     return (
         <Card className="dayCard">
-            <h1 class="dayName">{props.name}</h1>
+          <h1> {props.day} </h1>
+
+          <Card className="mealCard">
+            <h3> Breakfast:        </h3>
+            <h2> {meals.breakfast} </h2>
+          </Card>
+
+          <Card className="mealCard">
+            <h3> Lunch:        </h3>
+            <h2> {meals.lunch} </h2>
+          </Card>
+
+          <Card className="mealCard">
+            <h3> Dinner:        </h3>
+            <h2> {meals.dinner} </h2>
+          </Card>
+
         </Card>
     );
 }
 
 function Planner() {
   return (
-    <Stack direction="row" spacing={3}>
-      <DayCard name="Sunday"    />
-      <DayCard name="Monday"    />
-      <DayCard name="Tuesday"   />
-      <DayCard name="Wednesday" />
-      <DayCard name="Thursday"  />
-      <DayCard name="Friday"    />
-      <DayCard name="Saturday"  />
+    <Stack className="plannerStack" direction="row" spacing={3}>
+      <DayCard day="Sunday"    />
+      <DayCard day="Monday"    />
+      <DayCard day="Tuesday"   />
+      <DayCard day="Wednesday" />
+      <DayCard day="Thursday"  />
+      <DayCard day="Friday"    />
+      <DayCard day="Saturday"  />
     </Stack>
   );
 }
