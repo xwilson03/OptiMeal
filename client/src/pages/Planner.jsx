e3d34c662f5e9eeb80d48224ac724be1668855f3
xwilson03 <xanderwilson2014@gmail.com>
2023-10-22 02:42:50 -0500

    improves Planner css

--- client/src/pages/Planner.jsx
+++ client/src/pages/Planner.jsx
@@ -1,28 +1,25 @@
-import { Card, Container, Stack} from "@mui/material"
+import { Card, Stack} from "@mui/material"
 import "../styles/Planner.css"
 
 function DayCard(props) {
     return (
         <Card className="dayCard">
-            {props.name}
+            <h1 class="dayName">{props.name}</h1>
         </Card>
     );
 }
 
 function Planner() {
   return (
-    <div>
-      <p>Planner</p>
-      <Stack className="stack" direction="row" spacing={3}>
-        <DayCard name="Sunday"    />
-        <DayCard name="Monday"    />
-        <DayCard name="Tuesday"   />
-        <DayCard name="Wednesday" />
-        <DayCard name="Thursday"  />
-        <DayCard name="Friday"    />
-        <DayCard name="Saturday"  />
-      </Stack>
-    </div>
+    <Stack direction="row" spacing={3}>
+      <DayCard name="Sunday"    />
+      <DayCard name="Monday"    />
+      <DayCard name="Tuesday"   />
+      <DayCard name="Wednesday" />
+      <DayCard name="Thursday"  />
+      <DayCard name="Friday"    />
+      <DayCard name="Saturday"  />
+    </Stack>
   );
 }
 
