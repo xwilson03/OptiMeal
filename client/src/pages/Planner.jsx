5a28c30e0c7d0f155d97dad5923fdd36e17b37b3
xwilson03 <xanderwilson2014@gmail.com>
2023-10-22 00:12:55 -0500

    adds basic Planner layout

--- client/src/pages/Planner.jsx
+++ client/src/pages/Planner.jsx
@@ -1,9 +1,27 @@
-import "../styles/App.css";
+import { Card, Container, Stack} from "@mui/material"
+import "../styles/Planner.css"
+
+function DayCard(props) {
+    return (
+        <Card className="dayCard">
+            {props.name}
+        </Card>
+    );
+}
 
 function Planner() {
   return (
-    <div className="Planner">
+    <div>
       <p>Planner</p>
+      <Stack className="stack" direction="row" spacing={3}>
+        <DayCard name="Sunday"    />
+        <DayCard name="Monday"    />
+        <DayCard name="Tuesday"   />
+        <DayCard name="Wednesday" />
+        <DayCard name="Thursday"  />
+        <DayCard name="Friday"    />
+        <DayCard name="Saturday"  />
+      </Stack>
     </div>
   );
 }
